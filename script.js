b7094565958eb3b18617c63b18a4b92667c34738
PranithaVangala98 <108581187+PranithaVangala98@users.noreply.github.com>
2026-02-04 11:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(!str){
+		return null
+	}
+	let frechar = new Map()
+
+	for (let char of str) {
+		if(frechar.get(char)){
+			frechar.set(char, frechar.get(char)+1)
+		}else{
+			frechar.set(char, 1)
+		}	
+	}
+	for (let char of str) { 
+		if(frechar.get(char)===1){
+			return char
+		}
+	}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
